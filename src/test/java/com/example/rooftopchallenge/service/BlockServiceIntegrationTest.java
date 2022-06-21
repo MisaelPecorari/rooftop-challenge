52143e45d08c4764f2c962581eb8cd55f25910f9
Misael Pecorari <mpecorari@avaya.com>
2022-06-20 19:57:31 -0500

    use email from command line

--- src/test/java/com/example/rooftopchallenge/service/BlockServiceIntegrationTest.java
+++ src/test/java/com/example/rooftopchallenge/service/BlockServiceIntegrationTest.java
@@ -2,6 +2,7 @@ package com.example.rooftopchallenge.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.rooftopchallenge.client.BlockClient;
@@ -9,8 +10,10 @@ import com.example.rooftopchallenge.client.TokenClient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
+@Slf4j
 public class BlockServiceIntegrationTest {
 
     @Autowired
@@ -19,10 +22,13 @@ public class BlockServiceIntegrationTest {
     private BlockClient blockClient;
     @Autowired
     private BlockService blockService;
+    @Value("${api.email}")
+    private String email;
 
     @Test
     public void givenUnsortedBlocks_whenCheck_thenReturnSortedBlocks() {
-        String token = tokenClient.getToken("example@gmail.com");
+        log.info("email set {}", email);
+        String token = tokenClient.getToken(email);
         String[] unsortedBlocks = blockClient.getBlocks(token);
 
         String[] sortedBlocks = blockService.check(unsortedBlocks, token);
